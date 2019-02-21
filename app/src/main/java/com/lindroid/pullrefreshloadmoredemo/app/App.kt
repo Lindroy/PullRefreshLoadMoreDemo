ab1d49ea1ae5e1f6d154c32ee52c255a2c56285a
Lindroy <linyulong2014@163.com>
2019-02-21 17:59:04 +0800

    Initial commit

---
+++ app/src/main/java/com/lindroid/pullrefreshloadmoredemo/app/App.kt
@@ -0,0 +1,13 @@
+package com.lindroid.pullrefreshloadmoredemo.app
+
+import android.app.Application
+
+/**
+ * @author Lin
+ * @date 2019/2/21
+ * @function
+ * @Description
+ */
+class App:Application() {
+
+}
---
+++ app/src/main/java/com/lindroid/pullrefreshloadmoredemo/utils/AdapterUtil.kt
@@ -0,0 +1,39 @@
+package com.lindroid.pullrefreshloadmoredemo.utils
+
+import android.support.annotation.LayoutRes
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+
+/**
+ * @author Lin
+ * @date 2019/2/21
+ * @function BaseRecyclerViewAdapter工具类
+ * @Description
+ */
+
+/**
+ * 获取真实的头布局个数
+ */
+fun <T : Any> BaseQuickAdapter<T, BaseViewHolder>.getRealHeaderCount() =
+    if (headerLayout == null) 0 else headerLayout.childCount
+
+/**
+ * 获取真实的脚布局个数
+ */
+fun <T : Any> BaseQuickAdapter<T, BaseViewHolder>.getRealFooterCount() =
+    if (footerLayout == null) 0 else footerLayout.childCount
+
+
+/**
+ * Lambda表达式形式的BaseQuickAdapter类
+ * @param layoutId:列表item布局Id
+ */
+class BaseSimpleAdapter<T>(@LayoutRes layoutId: Int, private val convertListener: (helper: BaseViewHolder, item: T) -> Unit) :
+    BaseQuickAdapter<T, BaseViewHolder>(layoutId) {
+    override fun convert(helper: BaseViewHolder, item: T) {
+        convertListener.invoke(helper, item)
+    }
+
+}
+
+
